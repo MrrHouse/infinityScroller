40d793b790b34f209c9870ce4d6fd0c416fa7284
Tanner McBrayer <tmcbrayer@haynesfurniture.com>
2023-10-20 13:21:08 -0400

    Update 10/20/2023

---
+++ script.js
@@ -0,0 +1,18 @@
+// Unsplash API
+const count = 10;
+const apiKey = 'ZoUUqUcQEm_bDRINapvPqlO0LggvA5Rpq6t60DeEgMU';
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get Photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getPhotos();
